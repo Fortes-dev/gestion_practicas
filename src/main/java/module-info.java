cd8be3c28c627dca06603196125a76e91ccf6063
CarlosFortesMedina <CarlosFortesMedina@CS-EST-PC-13113>
2021-10-27 10:21:48 +0200

    Added data model classes and updated module-info.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,11 @@ module com.mycompany.gestion.practicas.application {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires org.hibernate.orm.core;
+    requires java.naming;
+    requires java.sql;
+    requires java.persistence;
 
-    opens com.mycompany.gestion.practicas.application to javafx.fxml;
+    opens com.mycompany.gestion.practicas.application to javafx.fxml, org.hibernate.orm.core, java.sql;
     exports com.mycompany.gestion.practicas.application;
 }
