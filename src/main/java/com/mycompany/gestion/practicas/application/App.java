61cd01d9ca98d16a42fac7f117323be6b7b611e7
Fortes-dev <medina.fortes.carlos@gmail.com>
2021-11-03 11:19:52 +0100

    Login fxml and controller
    Added both fxml and login controller.
    Added img and styles packages.

--- src/main/java/com/mycompany/gestion/practicas/application/App.java
+++ src/main/java/com/mycompany/gestion/practicas/application/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.scene.image.Image;
 
 /**
  * JavaFX App
@@ -17,7 +18,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("login"), 501, 660);
+        stage.setResizable(false);
+        stage.setTitle("CESUR Gestión de prácticas");
+        stage.getIcons().add(new Image("img/logo cesur.png"));
         stage.setScene(scene);
         stage.show();
     }
