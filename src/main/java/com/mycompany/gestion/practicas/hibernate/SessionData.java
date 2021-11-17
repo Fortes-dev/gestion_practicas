de0e4c08b622a01eaec3baadfdff4a4e1931aa75
Fortes-dev <medina.fortes.carlos@gmail.com>
2021-11-17 12:59:29 +0100

    Update SessionData.java
    Created SessionData to save data between views

--- src/main/java/com/mycompany/gestion/practicas/hibernate/SessionData.java
+++ src/main/java/com/mycompany/gestion/practicas/hibernate/SessionData.java
@@ -1,9 +1,18 @@
 package com.mycompany.gestion.practicas.hibernate;
 
+import com.mycompany.gestion.practicas.models.Alumno;
+import com.mycompany.gestion.practicas.models.Empresa;
+import com.mycompany.gestion.practicas.models.Practica;
+import com.mycompany.gestion.practicas.models.Profesor;
+
 
 
 public class SessionData {
 
+    private static Alumno alumnoActual;
+    private static Profesor profesorActual;
+    private static Practica practicaActual;
+    private static Empresa empresaActual;
 
-
+    
 }
