a63df27247f5e386273a6d9d7ba1a1861951ecf2
Fortes-dev <medina.fortes.carlos@gmail.com>
2021-12-18 01:07:00 +0100

    deleted constant

--- src/main/java/com/mycompany/gestion/practicas/customassets/Constants.java
+++ src/main/java/com/mycompany/gestion/practicas/customassets/Constants.java
@@ -11,7 +11,7 @@ package com.mycompany.gestion.practicas.customassets;
  */
 public class Constants {
     
-    public static final String API_KEY = "AIzaSyDckbK-ihblb2K5olTERcS_G6HnQ_3t4Ao";
+    public static final String API_KEY = "xxx";
     public static final String MAP_API_URL = "https://maps.googleapis.com/maps/api/staticmap?center=";
     public static final String MAP_API_MARKER = "&markers=color:red%7Clabel:S%7C";
     public static final String MAP_API_PARAMS = "&zoom=18&size=434x300&maptype=hybrid&key=";
